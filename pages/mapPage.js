f75632ccd9ce5e52b8bef6595b4dc85d82380935
raphaelobu <78567625+raphaelobu@users.noreply.github.com>
2021-11-02 14:32:03 -0400

    Test map scroll fix

--- pages/mapPage.js
+++ pages/mapPage.js
@@ -5,6 +5,7 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 42.352271, lng: -71.05524200000001 },
     zoom: 15,
+    gestureHandling: "greedy";
   });
 
   let user_lat;
